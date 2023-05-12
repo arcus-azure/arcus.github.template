e8319d43fef816dfed18427f92e560e686fe200c
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2023-05-12 12:11:50 +0200

    pr-fix: improve test config value guard

--- src/Arcus.Template.Tests.Integration/Configuration/TestConfig.cs
+++ src/Arcus.Template.Tests.Integration/Configuration/TestConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using GuardNet;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Primitives;
@@ -70,9 +71,28 @@ namespace Arcus.Template.Tests.Integration.Configuration
         /// </summary>
         /// <param name="key">The configuration key.</param>
         /// <returns>The configuration value.</returns>
+        /// <exception cref="KeyNotFoundException">Thrown when no value is found for the given <paramref name="key"/>.</exception>
+        /// <exception cref="InvalidOperationException">Thrown when the test configuration token value is not yet replaced by a real value.</exception>
         public string this[string key]
         {
-            get => _implementation[key] ?? throw new KeyNotFoundException($"Could not find test configuration value by key '{key}', make sure to add an entry in the 'appsettings.*.json' file");
+            get
+            {
+                string value = _implementation[key];
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new KeyNotFoundException(
+                        $"Cannot find any test configuration value for the key: '{key}', please make sure that this key is specified in your (local and remote) 'appsettings.json' file");
+                }
+
+                if (value.StartsWith("#{") && value.EndsWith("}#"))
+                {
+                    throw new InvalidOperationException(
+                        $"Cannot find configuration value for the key '{key}', as it is still having the token '{value}' and is not being replaced by the real value, " +
+                        "please make sure to add a local alternative in the 'appsettings.local.json' file for the token with the real value required for this key");
+                }
+
+                return value;
+            }
             set => _implementation[key] = value;
         }
     }
