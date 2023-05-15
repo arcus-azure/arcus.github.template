be32c75519760ffc026768ce5c076e72d259d182
Stijn Moreels <9039753+stijnmoreels@users.noreply.github.com>
2023-05-15 12:27:45 +0200

    chore: improve integration test base (#57)
    * FEAT: add NuGet YAML release boilerplate
    
    * chore: improve integration test base
    
    * pr-fix: add additional check for null test config value
    
    * pr-fix: update packages
    
    * pr-fix: improve test config value guard

--- src/Arcus.Template.Tests.Integration/IntegrationTest.cs
+++ src/Arcus.Template.Tests.Integration/IntegrationTest.cs
@@ -1,24 +1,35 @@
-﻿using Arcus.Template.Tests.Integration.Logging;
-using Microsoft.Extensions.Configuration;
+﻿using System;
+using Arcus.Template.Tests.Integration.Configuration;
+using Arcus.Testing.Logging;
+using Microsoft.Extensions.Logging;
 using Xunit.Abstractions;
 
 namespace Arcus.Template.Tests.Integration
 {
-    public class IntegrationTest
+    /// <summary>
+    /// Represents a common integration test in the test suite with easy access to common much-used types.
+    /// </summary>
+    public abstract class IntegrationTest
     {
-        protected IConfiguration Configuration { get; }
-        protected XunitTestLogger Logger { get; }
-
-        public IntegrationTest(ITestOutputHelper testOutput)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IntegrationTest"/> class.
+        /// </summary>
+        /// <param name="testOutput">The xUnit test output writer to log diagnostic messages to the test output.</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        protected IntegrationTest(ITestOutputHelper testOutput)
         {
+            Configuration = TestConfig.Create();
             Logger = new XunitTestLogger(testOutput);
-
-            // The appsettings.local.json allows users to override (gitignored) settings locally for testing purposes
-            Configuration = new ConfigurationBuilder()
-                .AddJsonFile(path: "appsettings.json")
-                .AddJsonFile(path: "appsettings.local.json", optional: true)
-                .AddEnvironmentVariables()
-                .Build();
         }
+
+        /// <summary>
+        /// Gets the test configuration instance to retrieve values and secrets required to interact with any external resources.
+        /// </summary>
+        protected TestConfig Configuration { get; }
+        
+        /// <summary>
+        /// Gets the logger instance to write diagnostic information messages during the test run.
+        /// </summary>
+        protected ILogger Logger { get; }
     }
 }
